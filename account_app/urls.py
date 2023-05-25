24697177949145f947cf6f9a46d0de4da445fba3
“shawazi” <shawazisonfire@gmail.com>
2023-05-25 05:42:40 -0400

    djdt was registered in the wrong urls.py; it needs to go in the project urls.py, not in the app urls.py

--- account_app/urls.py
+++ account_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from .views import RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.conf import settings
 
 urlpatterns = [
     path("register/", RegisterView.as_view()),
@@ -9,9 +8,3 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
--- main/settings/base.py
+++ main/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -22,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = get_random_secret_key()
 
 
 # Application definition
